72852e41cc5e09496f4ddfb3dc662f3ebcbc27b8
Robert-Iskandarani <riskandarani@fi.uba.ar>
2024-01-30 15:33:34 -0300

    se organiza codigo

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -7,6 +7,7 @@ export default function Navigation() {
       <div className='bg-blue-950 h-screen text-white flex flex-col '>
         <nav className='flex gap-4 items-center justify-start bg-white'>
           <img src='./logo.jpg' alt='React Logo' className='w-32' />
+
           <ul className='flex flex-row gap-14 text-black'>
             <li>
               <NavLink
@@ -34,7 +35,6 @@ export default function Navigation() {
             </li>
           </ul>
         </nav>
-
         <div className='h-full flex justify-center items-center text-8xl'>
           <Routes>
             <Route path='/' element={<h1>Home Page</h1>} />
